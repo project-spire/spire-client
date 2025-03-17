1f348857577e80b5e3ee7e1651e5e7b1e883f580
Sanito Gonzalez <ddalkisoup23@gmail.com>
2025-03-17 23:12:39 +0900

    Implement Session

--- client/network/NetProtocolHandler.cs
+++ client/network/NetProtocolHandler.cs
@@ -1,6 +1,27 @@
+using System;
+using System.Buffers;
+using spire.protocol.net;
+
 namespace spire.network;
 
 public class NetProtocolHandler
 {
+    public void Handle(NetProtocol protocol)
+    {
+        switch (protocol.ProtocolCase)
+        {
+            case NetProtocol.ProtocolOneofCase.Ping:
+                HandlePing();
+                break;
+            default:
+                break;
+        }
+    }
     
+    public void HandlePing()
+    {
+        var buffer = ArrayPool<byte>.Shared.Rent(size);
+
+        _ = channel.Writer.WriteAsync(buffer.AsMemory());
+    }
 }
